ca2f695e666d431e1181bb2865ed85ad382532a9
BukT0p <yak_vick@mail.ru>
2015-05-27 14:42:59 +0300

    Add ByteCode

--- README.md
+++ README.md
@@ -1,47 +1,4 @@
 # Tests
 Just a simple project to test simple things
 
-Byte Code for simple for:
 
-aload_1
-invokeinterface java/util/List/size()I 1
-istore_2
-iconst_0
-istore_3
-iconst_0
-istore 4
-iload 4
-iload_2
-if_icmpge 21
-iload_3
-aload_1
-iload 4
-invokeinterface java/util/List/get(I)Ljava/lang/Object; 2
-checkcast java/lang/String
-invokevirtual java/lang/String/length()I
-iadd
-istore_3
-iinc 4 1
-goto 8
-iload_3
-ireturn
-
-Byte Code for ForEach:
-iconst_0
-istore_2
-aload_1
-invokeinterface java/util/List/iterator()Ljava/util/Iterator; 1
-astore_3
-aload_3
-invokeinterface java/util/Iterator/hasNext()Z 1
-ifeq 17
-iload_2
-aload_3
-invokeinterface java/util/Iterator/next()Ljava/lang/Object; 1
-checkcast java/lang/String
-invokevirtual java/lang/String/length()I
-iadd
-istore_2
-goto 6
-iload_2
-ireturn
--- app/src/main/java/com/dataart/audiotest/TestActivity.java
+++ app/src/main/java/com/dataart/audiotest/TestActivity.java
@@ -2,7 +2,6 @@ package com.dataart.audiotest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +12,51 @@ import java.util.List;
 import java.util.Random;
 import java.util.Vector;
 
+/**Byte Code for simple for:
+
+aload_1
+invokeinterface java/util/List/size()I 1
+istore_2
+iconst_0
+istore_3
+iconst_0
+istore 4
+iload 4
+iload_2
+if_icmpge 21
+iload_3
+aload_1
+iload 4
+invokeinterface java/util/List/get(I)Ljava/lang/Object; 2
+checkcast java/lang/String
+invokevirtual java/lang/String/length()I
+iadd
+istore_3
+iinc 4 1
+goto 8
+iload_3
+ireturn
+
+Byte Code for ForEach:
+iconst_0
+istore_2
+aload_1
+invokeinterface java/util/List/iterator()Ljava/util/Iterator; 1
+astore_3
+aload_3
+invokeinterface java/util/Iterator/hasNext()Z 1
+ifeq 17
+iload_2
+aload_3
+invokeinterface java/util/Iterator/next()Ljava/lang/Object; 1
+checkcast java/lang/String
+invokevirtual java/lang/String/length()I
+iadd
+istore_2
+goto 6
+iload_2
+ireturn
+ */
 public class TestActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int ITEMS_NUMBER = 100000;
